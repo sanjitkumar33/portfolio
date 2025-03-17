e00a9a8b9ab14a155180f4a6f83707367babc009
sanjitkumar33 <kumarsan5233@gmail.com>
2025-03-17 16:19:56 +0530

    code update

--- profile.js
+++ profile.js
@@ -15,3 +15,16 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+// for canvas use only
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(0,0);
+ctx.lineTo(200,100);
+ctx.stroke();
+
+// for canvas1 use only
+var c = document.getElementById("myCanvas1");
+var ctx = c.getContext("2d");
+ctx.beginPath();
+ctx.arc(95,50,40,0,2*Math.PI);
+ctx.stroke();
